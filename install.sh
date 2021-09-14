e91ed32b64b43b4d58b79e7a7bb682e1fc613076
Aleksi Vesanto <avjves@gmail.com>
2021-09-14 19:11:39 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
-sudo apt install vim-nox build-essential cmake python3-dev npm nodejs  -y
-sudo apt install python3-neovim neovim -y
+sudo apt install build-essential cmake python3-dev  -y
+sudo apt install python3-neovim -y
 cd ~
 wget https://raw.githubusercontent.com/avjves/myVim/master/init.vim
 mkdir -p .config/nvim
-mv init.vim ../.config/nvim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mv init.vim .config/nvim
 cd .vim
 mkdir colors
 cd colors
 wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 cd ../
-vim +PluginInstall +qall
+nvim +PluginInstall +qall
 cd bundle
 cd youcompleteme
-python3 install.py --ts-completer
+python3 install.py
 cd ~
 echo "\nalias vim=nvim" >> .bashrc
